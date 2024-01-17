7deee9044540125196688ddd846895d6daa14399
DanilloFonseca <danillo.fonseca.129@ufrn.edu.br>
2024-01-16 23:14:03 -0300

    feat: complete

--- src/logic.lean
+++ src/logic.lean
@@ -13,8 +13,6 @@ by
   intro np
   contradiction
 
-section
-open Classical
 
 theorem doubleneg_elim :
   ¬¬P → P  :=
@@ -23,9 +21,6 @@ by
   apply Classical.byContradiction
   intro np
   contradiction
-
-end
-
 
 
 theorem doubleneg_law :
@@ -108,7 +103,6 @@ by
   contradiction
 
 
-
 theorem contrapositive_law :
   (P → Q) ↔ (¬Q → ¬P)  :=
 by
@@ -149,12 +143,14 @@ by
 
 
   theorem peirce_law :
-  ((P → Q) → P) → P  := by
-  intro hpqp
-  have hpqpnnp: (((P → Q) → P) → ¬¬P) := peirce_law_weak P Q
-  have notnotp: ¬¬P := hpqpnnp hpqp
-  have hnnpp: (¬¬P → P) := doubleneg_elim P
-  exact hnnpp notnotp
+  ((P → Q) → P) → P  :=
+by
+  intro pqp
+  have peirceLaw: (((P → Q) → P) → ¬¬P) := peirce_law_weak P Q
+  have nnp: ¬¬P := peirceLaw pqp
+  apply Classical.byContradiction
+  intro np
+  contradiction
 
 
 ------------------------------------------------
@@ -204,6 +200,7 @@ by
   apply npq
   apply Or.inr
   assumption
+
 
 theorem demorgan_disj_converse :
   (¬P ∧ ¬Q) → ¬(P∨Q)  :=
@@ -255,6 +252,7 @@ by
   exact demorgan_conj P Q
   exact demorgan_conj_converse P Q
 
+
 theorem demorgan_disj_law :
   ¬(P∨Q) ↔ (¬P ∧ ¬Q)  :=
 by
@@ -300,7 +298,6 @@ by
   exact And.right pr
 
 
-
 theorem distr_disj_conj :
   P∨(Q∧R) → (P∨Q)∧(P∨R)  :=
 by
@@ -318,6 +315,7 @@ by
   exact And.left qr
   apply Or.inr
   exact And.right qr
+
 
 theorem distr_disj_conj_converse :
   (P∨Q)∧(P∨R) → P∨(Q∧R)  :=
@@ -339,7 +337,6 @@ by
   assumption
 
 
-
 ------------------------------------------------
 -- Currificação
 ------------------------------------------------
@@ -354,6 +351,7 @@ by
   apply And.intro
   assumption
   assumption
+
 
 theorem uncurry_prop :
   (P→(Q→R)) → ((P∧Q)→R)  :=
@@ -386,6 +384,7 @@ by
   apply Or.inl
   assumption
 
+
 theorem weaken_disj_left :
   Q → (P∨Q)  :=
 by
@@ -393,17 +392,20 @@ by
   apply Or.inr
   assumption
 
+
 theorem weaken_conj_right :
   (P∧Q) → P  :=
 by
   intro pq
   exact And.left pq
 
+
 theorem weaken_conj_left :
   (P∧Q) → Q  :=
 by
   intro pq
   exact And.right pq
+
 
 theorem conj_idempot :
   (P∧P) ↔ P :=
@@ -415,6 +417,7 @@ by
   apply And.intro
   assumption
   assumption
+
 
 theorem disj_idempot :
   (P∨P) ↔ P  :=
@@ -429,6 +432,7 @@ by
   intro p
   apply Or.inl
   assumption
+
 
 end propositional
 
@@ -455,6 +459,7 @@ by
   apply Exists.intro x
   assumption
 
+
 theorem demorgan_exists_converse :
   (∀x, ¬P x) → ¬(∃x, P x)  :=
 by
@@ -465,6 +470,7 @@ by
   intro pxx
   have npx: ¬P x := h x
   contradiction
+
 
 theorem demorgan_forall :
   ¬(∀x, P x) → (∃x, ¬P x)  :=
@@ -480,6 +486,7 @@ by
   apply Exists.intro x
   assumption
 
+
 theorem demorgan_forall_converse :
   (∃x, ¬P x) → ¬(∀x, P x)  :=
 by
@@ -491,12 +498,14 @@ by
   have px: P x := fpx x
   contradiction
 
+
 theorem demorgan_forall_law :
   ¬(∀x, P x) ↔ (∃x, ¬P x)  :=
 by
   apply Iff.intro
   exact demorgan_forall U P
   exact demorgan_forall_converse U P
+
 
 theorem demorgan_exists_law :
   ¬(∃x, P x) ↔ (∀x, ¬P x)  :=
@@ -521,6 +530,7 @@ by
   have npx: ¬P x := hp x
   contradiction
 
+
 theorem forall_as_neg_exists :
   (∀x, P x) → ¬(∃x, ¬P x)  :=
 by
@@ -532,6 +542,7 @@ by
   have px: P x := h x
   contradiction
 
+
 theorem forall_as_neg_exists_converse :
   ¬(∃x, ¬P x) → (∀x, P x)  :=
 by
@@ -542,6 +553,7 @@ by
   suffices hp: (∃x, ¬P x) from False.elim (h hp)
   apply Exists.intro x
   assumption
+
 
 theorem exists_as_neg_forall_converse :
   ¬(∀x, ¬P x) → (∃x, P x)  :=
@@ -564,6 +576,7 @@ by
   exact forall_as_neg_exists U P
   exact forall_as_neg_exists_converse U P
 
+
 theorem exists_as_neg_forall_law :
   (∃x, P x) ↔ ¬(∀x, ¬P x)  :=
 by
@@ -579,33 +592,92 @@ by
 theorem exists_conj_as_conj_exists :
   (∃x, P x ∧ Q x) → (∃x, P x) ∧ (∃x, Q x)  :=
 by
-  sorry
+  intro ⟨x,q⟩
+  apply And.intro
+  apply Exists.intro x
+  exact And.left q
+  apply Exists.intro x
+  exact And.right q
+
 
 theorem exists_disj_as_disj_exists :
   (∃x, P x ∨ Q x) → (∃x, P x) ∨ (∃x, Q x)  :=
 by
-  sorry
+  intro h
+  apply Exists.elim h
+  intro x
+  intro hpp
+  apply Or.elim hpp
+  intro px
+  apply Or.inl
+  apply Exists.intro x
+  assumption
+  intro qx
+  apply Or.inr
+  apply Exists.intro x
+  assumption
+
 
 theorem exists_disj_as_disj_exists_converse :
   (∃x, P x) ∨ (∃x, Q x) → (∃x, P x ∨ Q x)  :=
 by
-  sorry
+  intro h
+  apply Or.elim h
+  intro epx
+  apply Exists.elim epx
+  intro x
+  intro px
+  apply Exists.intro x
+  apply Or.inl
+  assumption
+  intro eqx
+  apply Exists.elim eqx
+  intro x
+  intro qx
+  apply Exists.intro x
+  apply Or.inr
+  assumption
+
 
 theorem forall_conj_as_conj_forall :
   (∀x, P x ∧ Q x) → (∀x, P x) ∧ (∀x, Q x)  :=
 by
-  sorry
+  intro h
+  apply And.intro
+  intro x
+  have pxqx: P x ∧ Q x := h x
+  exact And.left pxqx
+  intro x
+  have pxqx: P x ∧ Q x := h x
+  exact And.right pxqx
+
 
 theorem forall_conj_as_conj_forall_converse :
   (∀x, P x) ∧ (∀x, Q x) → (∀x, P x ∧ Q x)  :=
 by
-  sorry
+  intro h
+  intro x
+  have px: P x := (And.left h) x
+  apply And.intro
+  assumption
+  have qx: Q x := (And.right h) x
+  assumption
 
 
 theorem forall_disj_as_disj_forall_converse :
   (∀x, P x) ∨ (∀x, Q x) → (∀x, P x ∨ Q x)  :=
 by
-  sorry
+  intro h
+  intro x
+  apply Or.elim h
+  intro fpx
+  have px: P x := fpx x
+  apply Or.inl
+  assumption
+  intro fqx
+  have qx: Q x := fqx x
+  apply Or.inr
+  assumption
 
 
 /- NOT THEOREMS --------------------------------
